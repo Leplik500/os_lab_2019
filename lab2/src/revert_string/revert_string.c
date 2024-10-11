72e67c765c75f3f3699fda660c5a64f6a0070986
naotomo <nikitin-ao-dev@yandex.ru>
2024-10-11 22:15:27 +0300

    implement Swap() and ReverseString()

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,20 @@
 #include "revert_string.h"
+#include <stdlib.h>
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    if (str == NULL)
+        return;
+
+    int i, j;
+    char temp;
+
+    for (i = 0, j = strlen(str) - 1; i < j; i++, j--)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
 }
 
